1fc2c95ddcf4439fb66165227dc0062f9e12c9b0
TheFilip <121570139+TheFilipcom4607@users.noreply.github.com>
2023-06-11 10:35:55 +0100

    Update analytics.tsx

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -6,10 +6,6 @@ export function Analytics() {
 		return null;
 	}
 	return (
-		<script
-			src="https://beamanalytics.b-cdn.net/beam.min.js"
-			data-token={token}
-			async
-		/>
+		<script src="https://cdn.counter.dev/script.js" data-id="e996993b-acbc-437e-9894-684cb0b07a62" data-utcoffset="1"></script>
 	);
 }

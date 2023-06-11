52d6900c44c066c44e1fcdf8aa7cff79e94e4b51
TheFilip <121570139+TheFilipcom4607@users.noreply.github.com>
2023-06-11 11:29:51 +0100

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -46,10 +46,7 @@ export default async function ProjectsPage() {
 				<div className="max-w-2xl mx-auto lg:mx-0">
 					<h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
 						Projects
-					</h2>
-					<p className="mt-4 text-zinc-400">
-						Some of the projects are from work and some are on my own time.
-					</p>
+					</h2> 
 				</div>
 				<div className="w-full h-px bg-zinc-800" />
 

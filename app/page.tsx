72e4400d7b7bde34fcf308f70303ee5606767d49
TheFilip <121570139+TheFilipcom4607@users.noreply.github.com>
2023-06-11 10:39:36 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,23 +35,12 @@ export default function Home() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Hi, i am TheFilip, I'm building serverless and open source
-					solutions at{" "}
-					<Link
-						target="_blank"
-						href="https://upstash.com"
-						className="underline duration-500 hover:text-zinc-300"
-					>
-						Upstash
-					</Link>
-
+					Hi, i am TheFilip. I'm making various things, mainly in python.
+     
+					
 					<br />
-					and working on{" "}
-					<Link
-						target="_blank"
-						href="https://planetfall.io"
-						className="underline duration-500 hover:text-zinc-300"
-					>
+					and working on
+			
 						planetfall.io
 					</Link>{" "}
 					at night.

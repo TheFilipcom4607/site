fdf63515751f3134dd96a30243be8532461f52da
TheFilip <121570139+TheFilipcom4607@users.noreply.github.com>
2024-01-11 21:34:10 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Hi, I am TheFilip. I make various things, mainly in Python and work with Raspberry Pi at night.
+					Hello, I'm TheFilip. I like 3d printing and tech. I enjoy working on raspberry pi projects.
 				</h2>
 			</div>
 		</div>
